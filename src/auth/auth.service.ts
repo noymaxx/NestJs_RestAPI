7be91063d40660d9254d0fab77357714625043c5
noymaxx <hug.povoleri@sou.inteli.edu.br>
2023-11-28 00:51:35 -0300

    feat: finish signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -10,15 +11,31 @@ export class AuthService {
     // generate the password hash
     const hash = await argon.hash(dto.password);
     // save te new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
+    try
+    {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
     // return the saved user
     return user;
+    }
+    catch(error)
+    {
+      if(error instanceof PrismaClientKnownRequestError)
+      {
+        if(error.code === 'P2002')
+        {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
+
   signin() {
     return 'login';
   }
