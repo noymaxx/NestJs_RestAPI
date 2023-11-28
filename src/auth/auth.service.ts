f690098ed5e6708e6124b70022168db918103617
noymaxx <hug.povoleri@sou.inteli.edu.br>
2023-11-28 00:20:34 -0300

    fix: PORTS docker and .env

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
 @Injectable()
 export class AuthService {
-
-constructor(private prisma: PrismaService) {}
-
-  signin() {
-    return "login";
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save te new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // return the saved user
+    return user;
   }
-  signup(dto: Authdto) {
-    return "signup";
+  signin() {
+    return 'login';
   }
 }
